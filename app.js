5650e9f4fe1105249ff7ef28848d7f33a9dc899f
grzegorz103 <grzesiek555_12@outlook.com>
2022-05-11 20:36:15 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var stompClient = null;
 
 function connect() {
-    var socket = new SockJS('http://localhost:8080/websocket');
+    var socket = new SockJS('http://localhost:8080/websocket', null, {sessionId: 128});
     stompClient = Stomp.over(socket);
-	
+
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/test-topic', function (message) {
